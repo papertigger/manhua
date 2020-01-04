c48236db3a18966fed85fac1be90104733cf4a3f
纸飞机 <2475314660@qq.com>
2020-01-04 14:27:56 +0800

    a manhua app for vue doing

---
+++ precache-manifest.01b873437d6575011e9b4faaa23af033.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c20cce37e7b56c24f31",
+    "url": "css/chunk-cd09ad64.b73208f3.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "0c20cce37e7b56c24f31",
+    "url": "js/chunk-cd09ad64.457f0fe7.js"
+  },
+  {
+    "revision": "a8b96aa4e0d45eae5561",
+    "url": "css/chunk-01dbb11b.d1541db3.css"
+  },
+  {
+    "revision": "f2ec315f15f2a54cf77d",
+    "url": "css/chunk-63b119d4.56d21659.css"
+  },
+  {
+    "revision": "f2ec315f15f2a54cf77d",
+    "url": "js/chunk-63b119d4.eccb8766.js"
+  },
+  {
+    "revision": "ed781d5ef4966afe7906",
+    "url": "css/chunk-6c54696e.649a3872.css"
+  },
+  {
+    "revision": "ed781d5ef4966afe7906",
+    "url": "js/chunk-6c54696e.47977c34.js"
+  },
+  {
+    "revision": "6de860824d4f91346398",
+    "url": "js/app.50d8abac.js"
+  },
+  {
+    "revision": "a8b96aa4e0d45eae5561",
+    "url": "js/chunk-01dbb11b.38d4f322.js"
+  },
+  {
+    "revision": "9c3ea60796bf7403d70f",
+    "url": "css/chunk-vendors.728eb7d9.css"
+  },
+  {
+    "revision": "9c3ea60796bf7403d70f",
+    "url": "js/chunk-vendors.f490152b.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "6c3da3a6f4d972f492921bd465425dd1",
+    "url": "img/tag.6c3da3a6.png"
+  },
+  {
+    "revision": "4355e953dab7f4ff970393f1b5794be8",
+    "url": "index.html"
+  },
+  {
+    "revision": "6de860824d4f91346398",
+    "url": "css/app.edce13d3.css"
+  }
+];
